7755ce0f336ab618568d4c82b52e74136db020d8
Adapt3R <adapt3r@gmail.com>
2025-06-15 18:27:23 -0700

    ready to debug action transformer refactor

--- imitation/algos/utils/rotation_transformer.py
+++ imitation/algos/utils/rotation_transformer.py
@@ -2,7 +2,7 @@
 Adapted from https://github.com/real-stanford/diffusion_policy/blob/main/diffusion_policy/model/common/rotation_transformer.py
 """
 
-from typing import Union
+from typing import Union, Literal, Callable
 import imitation.utils.pytorch3d_transforms as pt
 import torch
 import numpy as np
@@ -16,6 +16,28 @@ class RotationTransformer:
         'rotation_6d',
         'matrix'
     ]
+
+    # Dictionary mapping rotation representations to their sizes
+    rep_sizes = {
+        'matrix': 9,
+        'quaternion': 4,
+        'rotation_6d': 6,
+        'axis_angle': 3,
+        'euler_angles': 3
+    }
+
+    @classmethod
+    def get_rep_size(cls, rep: str) -> int:
+        """Get the size of a rotation representation.
+        
+        Args:
+            rep: One of the valid rotation representations
+            
+        Returns:
+            int: Size of the representation
+        """
+        assert rep in cls.valid_reps, f"Invalid representation {rep}"
+        return cls.rep_sizes[rep]
 
     def __init__(self, 
             rep_in='axis_angle', 
@@ -42,6 +64,13 @@ class RotationTransformer:
         assert rep_network in self.valid_reps
         assert rep_out in self.valid_reps
 
+        self.rep_in = rep_in
+        self.rep_network = rep_network
+        self.rep_out = rep_out
+        self.convention_in = convention_in
+        self.convention_network = convention_network
+        self.convention_out = convention_out
+
         if rep_in == 'euler_angles':
             assert convention_in is not None
         if rep_network == 'euler_angles':
@@ -49,31 +78,112 @@ class RotationTransformer:
         if rep_out == 'euler_angles':
             assert convention_out is not None
 
-        # Setup preprocessing (in -> network)
-        self.preprocess_funcs = []
-        if rep_in != rep_network:
-            funcs = [
-                getattr(pt, f'{rep_in}_to_matrix'),
-                getattr(pt, f'matrix_to_{rep_network}')
-            ]
-            if convention_in is not None:
-                funcs[0] = functools.partial(funcs[0], convention=convention_in)
-            if convention_network is not None:
-                funcs[1] = functools.partial(funcs[1], convention=convention_network)
-            self.preprocess_funcs = funcs
+    def _get_conversion_func(self, from_rep: str, to_rep: str):
+        """Get the appropriate conversion function between two representations.
+        
+        Args:
+            from_rep: Source representation
+            to_rep: Target representation
+            
+        Returns:
+            Callable: Function to convert between representations
+        """
+        if from_rep == to_rep:
+            return lambda x: x
+            
+        if from_rep == 'matrix':
+            func = getattr(pt, f'matrix_to_{to_rep}')
+            if to_rep == 'euler_angles':
+                convention = getattr(self, f'convention_{to_rep}')
+                func = functools.partial(func, convention=convention)
+        elif to_rep == 'matrix':
+            func = getattr(pt, f'{from_rep}_to_matrix')
+            if from_rep == 'euler_angles':
+                convention = getattr(self, f'convention_{from_rep}')
+                func = functools.partial(func, convention=convention)
+        else:
+            # Convert through matrix as intermediate
+            func1 = getattr(pt, f'{from_rep}_to_matrix')
+            func2 = getattr(pt, f'matrix_to_{to_rep}')
+            if from_rep == 'euler_angles':
+                convention = getattr(self, f'convention_{from_rep}')
+                func1 = functools.partial(func1, convention=convention)
+            if to_rep == 'euler_angles':
+                convention = getattr(self, f'convention_{to_rep}')
+                func2 = functools.partial(func2, convention=convention)
+            func = lambda x: func2(func1(x))
+            
+        return func
 
-        # Setup postprocessing (network -> out)
-        self.postprocess_funcs = []
-        if rep_network != rep_out:
-            funcs = [
-                getattr(pt, f'{rep_network}_to_matrix'),
-                getattr(pt, f'matrix_to_{rep_out}')
-            ]
-            if convention_network is not None:
-                funcs[0] = functools.partial(funcs[0], convention=convention_network)
-            if convention_out is not None:
-                funcs[1] = functools.partial(funcs[1], convention=convention_out)
-            self.postprocess_funcs = funcs
+    def convert(self, x: Union[np.ndarray, torch.Tensor], 
+                from_rep: Union[Literal['input', 'network', 'output'], str], 
+                to_rep: Union[Literal['input', 'network', 'output'], str]) -> Union[np.ndarray, torch.Tensor]:
+        """Convert between any two representations.
+        
+        Args:
+            x: Input rotation representation
+            from_rep: Source representation ('input', 'network', 'output', or any valid representation)
+            to_rep: Target representation ('input', 'network', 'output', or any valid representation)
+            
+        Returns:
+            Converted rotation representation
+        """
+        if self.identity:
+            return x
+            
+        # Map stage names to actual representations
+        rep_map = {
+            'input': self.rep_in,
+            'network': self.rep_network,
+            'output': self.rep_out
+        }
+        
+        # Get actual representations, handling both stage names and direct representation names
+        from_rep_actual = rep_map.get(from_rep, from_rep)
+        to_rep_actual = rep_map.get(to_rep, to_rep)
+        
+        # Validate representations
+        assert from_rep_actual in self.valid_reps, f"Invalid source representation: {from_rep}"
+        assert to_rep_actual in self.valid_reps, f"Invalid target representation: {to_rep}"
+        
+        func = self._get_conversion_func(from_rep_actual, to_rep_actual)
+        return self._apply_funcs(x, [func])
+
+    def __getattr__(self, name: str) -> Callable:
+        """Dynamically handle conversion method names.
+        
+        Examples:
+            - network_to_matrix: Convert from network representation to matrix
+            - input_to_output: Convert from input representation to output representation
+            - matrix_to_network: Convert from matrix to network representation
+            
+        Args:
+            name: Method name in format 'from_to'
+            
+        Returns:
+            Callable: Function that performs the requested conversion
+        """
+        if '_to_' not in name:
+            return super().__getattr__(name)
+            
+        from_rep, to_rep = name.split('_to_')
+        
+        def conversion_func(x: Union[np.ndarray, torch.Tensor]) -> Union[np.ndarray, torch.Tensor]:
+            return self.convert(x, from_rep=from_rep, to_rep=to_rep)
+            
+        return conversion_func
+
+    def get_input_size(self) -> int:
+        """Get the size of the input representation."""
+        return self.get_rep_size(self.rep_in)
+
+    def get_network_size(self) -> int:
+        """Get the size of the network representation."""
+        return self.get_rep_size(self.rep_network)
+
+    def get_output_size(self) -> int:
+        """Get the size of the output representation."""
+        return self.get_rep_size(self.rep_out)
 
     @staticmethod
     def _apply_funcs(x: Union[np.ndarray, torch.Tensor], funcs: list) -> Union[np.ndarray, torch.Tensor]:
@@ -94,16 +204,12 @@ class RotationTransformer:
     def preprocess(self, x: Union[np.ndarray, torch.Tensor]
         ) -> Union[np.ndarray, torch.Tensor]:
         """Transform from input representation to network representation"""
-        if self.identity:
-            return x
-        return self._apply_funcs(x, self.preprocess_funcs)
+        return self.convert(x, from_rep='input', to_rep='network')
     
     def postprocess(self, x: Union[np.ndarray, torch.Tensor]
         ) -> Union[np.ndarray, torch.Tensor]:
         """Transform from network representation to output representation"""
-        if self.identity:
-            return x
-        return self._apply_funcs(x, self.postprocess_funcs)
+        return self.convert(x, from_rep='network', to_rep='output')
 
 
 def test():
